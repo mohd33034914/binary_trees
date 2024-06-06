87968637cf1b7f6e3bfcf88d5fd386af0ee9c9da
Muhammad_Kabiru_Sani <mohd33034914@gmail.com>
2024-06-06 16:41:42 -0700

    trrss

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,7 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
 
-#include <stddef.h>
-
+#include <stdio.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,14 +10,20 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
 
-#endif /* _BINARY_TREES_H_ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+
+#endif
